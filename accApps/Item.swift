62a91de7d43e42269691679728bbb583f18fa443
Rana <rana.binmadi@gmail.com>
2025-11-22 15:34:08 +0300

    Initial Commit

---
+++ accApps/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  accApps
+//
+//  Created by Rana on 01/06/1447 AH.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ accApps/accAppsApp.swift
@@ -0,0 +1,32 @@
+//
+//  accAppsApp.swift
+//  accApps
+//
+//  Created by Rana on 01/06/1447 AH.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct accAppsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
